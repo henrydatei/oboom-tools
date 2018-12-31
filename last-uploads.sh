3c18dee030fea46199d7808a4471ae85c283bfdc
henrydatei <henrydatei@web.de>
2018-12-31 22:17:24 +0100

    added last-uploads

---
+++ last-uploads.sh
@@ -0,0 +1,25 @@
+read -p "session id: " session
+
+i=3
+
+state=$(curl -s -d "token=$session" "https://api.oboom.com/1/remote/lsall" | jq -r ".[$i] | .[0].state")
+
+while [ "$state" != "null" ]; do
+  curl -s -d "token=$session" "https://api.oboom.com/1/remote/lsall" >> upload-state.txt
+  state=$(cat upload-state.txt | jq -r ".[$i] | .[0].state")
+  url=$(cat upload-state.txt | jq -r ".[$i] | .[0].url")
+  if [ "$state" == "complete" ]; then
+    name=$(cat upload-state.txt | jq -r ".[$i] | .[0].name")
+    size=$(cat upload-state.txt | jq -r ".[$i] | .[0].size")
+    item=$(cat upload-state.txt | jq -r ".[$i] | .[0].item")
+    echo "\033[32m$state\033[0m $name ($item), size: $size, url: \033[34m$url\033[0m"
+  else
+    loadedsize=$(cat upload-state.txt | jq -r ".[$i] | .[0].loaded_size")
+    speed=$(cat upload-state.txt | jq -r ".[$i] | .[0].speed")
+    echo "\033[33m$state\033[0m $name ($item), loaded size: $loadedsize, speed: $speed, url: \033[34m$url\033[0m"
+  fi
+  i=$(($i + 1))
+  state=$(curl -s -d "token=$session" "https://api.oboom.com/1/remote/lsall" | jq -r ".[$i] | .[0].state")
+done
+
+rm upload-state.txt
--- remote-upload.sh
+++ remote-upload.sh
@@ -6,4 +6,4 @@ echo "Creating folder \033[32m$folder\033[0m."
 folderid=$(curl -s "https://api.oboom.com/1/mkdir?token=$session&parent=1&name=$folder" | jq -r '.[1]')
 echo "Created folder \033[32m$folder\033[0m with id \033[32m$folderid\033[0m."
 curl -s -d "token=$session" -d "remotes=[{\"url\":\"$link\",\"parent\":\"$folderid\"}]" "https://api.oboom.com/1/remote/add" >> /dev/null
-echo "Upload successful."
+echo "Upload (hopefully) successful. See last-uploads.sh for more details."
