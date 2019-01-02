60f628a172b208b6a482a0b4813c8deccd7ded67
henrydatei <henrydatei@web.de>
2019-01-02 18:26:57 +0100

    removed small error

--- remote-upload-list.sh
+++ remote-upload-list.sh
@@ -19,9 +19,7 @@ fi
 read -p "name of the folder (please no spaces): " folder
 read -p "list of files: " file
 
-if [ -n -f $file ]; then
-  echo "File \033[32m$file\033[0m does not exists."
-else
+if [ -f $file ]; then
   echo "Creating folder \033[32m$folder\033[0m."
   folderid=$(curl -s "https://api.oboom.com/1/mkdir?token=$session&parent=1&name=$folder" | jq -r '.[1]')
   echo "Created folder \033[32m$folder\033[0m with id \033[32m$folderid\033[0m."
@@ -29,5 +27,7 @@ else
     echo "Started upload of \033[34m$line\033[0m"
     curl -s -d "token=$session" -d "remotes=[{\"url\":\"$line\",\"parent\":\"$folderid\"}]" "https://api.oboom.com/1/remote/add" >> /dev/null
   done
+else
+  echo "File \033[32m$file\033[0m does not exists."
 fi
 echo "Uploads (hopefully) successful. See last-uploads.sh for more details."
