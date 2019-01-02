e9b6b61677f157a01d1be36c1af5d573bc09fe59
henrydatei <henrydatei@web.de>
2019-01-02 16:44:28 +0100

    improved auto-login

--- last-uploads.sh
+++ last-uploads.sh
@@ -1,12 +1,14 @@
 if [ -f session.txt ]; then
   timestampnow=$(date +%s)
   timestampold=$(cat session.txt | cut -d "|" -f1)
-  difference=$(($timestampnow - $timestampold))
+  difference=$(echo "$timestampnow - $timestampold" | bc)
   if [ $difference -le 1800 ]; then
     #session-id is only 30 minutes = 1800 seconds valid.
     session=$(cat session.txt | cut -d "|" -f2)
   else
     echo "Your session-ID is not valid anymore. Please get a new one with login.sh"
+    rm session.txt
+    exit
   fi
 else
   echo "File \033[32msession.txt\033[0m not found."
@@ -30,7 +32,7 @@ while [ "$state" != "null" ]; do
   else
     loadedsize=$(cat upload-state.txt | jq -r ".[$i] | .[0].loaded_size")
     speed=$(cat upload-state.txt | jq -r ".[$i] | .[0].speed")
-    echo "\033[33m$state\033[0m $name ($item), loaded size: $loadedsize, speed: $speed, url: \033[34m$url\033[0m"
+    echo "\033[33m$state\033[0m, loaded size: $loadedsize, speed: $speed, url: \033[34m$url\033[0m"
   fi
   i=$(($i + 1))
   state=$(curl -s -d "token=$session" "https://api.oboom.com/1/remote/lsall" | jq -r ".[$i] | .[0].state")
--- remote-upload.sh
+++ remote-upload.sh
@@ -1,12 +1,14 @@
 if [ -f session.txt ]; then
   timestampnow=$(date +%s)
   timestampold=$(cat session.txt | cut -d "|" -f1)
-  difference=$(($timestampnow - $timestampold))
+  difference=$(echo "$timestampnow - $timestampold" | bc)
   if [ $difference -le 1800 ]; then
     #session-id is only 30 minutes = 1800 seconds valid.
     session=$(cat session.txt | cut -d "|" -f2)
   else
     echo "Your session-ID is not valid anymore. Please get a new one with login.sh"
+    rm session.txt
+    exit
   fi
 else
   echo "File \033[32msession.txt\033[0m not found."
