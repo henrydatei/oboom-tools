84524105e76cdf6efa011b8f3dc1addd14ffef01
Henry Haustein <henrydatei@web.de>
2020-03-07 13:54:22 +0100

    added force-mode for restart-uploads.sh

--- restart-uploads.sh
+++ restart-uploads.sh
@@ -21,18 +21,34 @@ i=0
 curl -s -d "token=$session" "https://api.oboom.com/1/remote/lsall" >> upload-state.txt
 state=$(cat upload-state.txt | jq -r ".[3] | .[$i].state")
 
-while [ "$state" != "null" ]; do
-  state=$(cat upload-state.txt | jq -r ".[3] | .[$i].state")
-  if [ "$state" = "failed" ] || [ "$state" = "retry" ]; then
-    url=$(cat upload-state.txt | jq -r ".[3] | .[$i].url")
-    id=$(cat upload-state.txt | jq -r ".[3] | .[$i].id")
-    parent=$(cat upload-state.txt | jq -r ".[3] | .[$i].parent")
-    echo "Restarting url: \033[34m$url\033[0m, id: $id, parent: $parent"
-    curl -s "https://api.oboom.com/1/remote/rm?token=$session&remotes=$id" >> /dev/null
-    curl -s -d "token=$session" -d "remotes=[{\"url\":\"$url\",\"parent\":\"$parent\"}]" "https://api.oboom.com/1/remote/add" >> /dev/null
-  fi
-  i=$(($i + 1))
-  state=$(cat upload-state.txt | jq -r ".[3] | .[$i].state")
-done
+if [ "$1" = "force" ]; then
+  while [ "$state" != "null" ]; do
+    state=$(cat upload-state.txt | jq -r ".[3] | .[$i].state")
+    if [ "$state" = "failed" ] || [ "$state" = "retry" ] || [ "$state" = "pending" ]; then
+      url=$(cat upload-state.txt | jq -r ".[3] | .[$i].url")
+      id=$(cat upload-state.txt | jq -r ".[3] | .[$i].id")
+      parent=$(cat upload-state.txt | jq -r ".[3] | .[$i].parent")
+      echo "Restarting url: \033[34m$url\033[0m, id: $id, parent: $parent"
+      curl -s "https://api.oboom.com/1/remote/rm?token=$session&remotes=$id" >> /dev/null
+      curl -s -d "token=$session" -d "remotes=[{\"url\":\"$url\",\"parent\":\"$parent\"}]" "https://api.oboom.com/1/remote/add" >> /dev/null
+    fi
+    i=$(($i + 1))
+    state=$(cat upload-state.txt | jq -r ".[3] | .[$i].state")
+  done
+else
+  while [ "$state" != "null" ]; do
+    state=$(cat upload-state.txt | jq -r ".[3] | .[$i].state")
+    if [ "$state" = "failed" ] || [ "$state" = "retry" ]; then
+      url=$(cat upload-state.txt | jq -r ".[3] | .[$i].url")
+      id=$(cat upload-state.txt | jq -r ".[3] | .[$i].id")
+      parent=$(cat upload-state.txt | jq -r ".[3] | .[$i].parent")
+      echo "Restarting url: \033[34m$url\033[0m, id: $id, parent: $parent"
+      curl -s "https://api.oboom.com/1/remote/rm?token=$session&remotes=$id" >> /dev/null
+      curl -s -d "token=$session" -d "remotes=[{\"url\":\"$url\",\"parent\":\"$parent\"}]" "https://api.oboom.com/1/remote/add" >> /dev/null
+    fi
+    i=$(($i + 1))
+    state=$(cat upload-state.txt | jq -r ".[3] | .[$i].state")
+  done
+fi
 
 rm upload-state.txt
